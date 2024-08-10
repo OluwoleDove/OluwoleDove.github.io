f3cbbfc141ac8c965c5e59f1d752aabd58967a08
OluwoleDove <ladmans07@gmail.com>
2024-08-10 05:59:55 +0100

    Creating scrapy edtech

---
+++ edtech_scrapy/frontend/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { courseListReducer, courseDetailsReducer } from './reducers/courseReducers';
+
+const reducer = combineReducers({
+    courseList: courseListReducer,
+    courseDetails: courseDetailsReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
