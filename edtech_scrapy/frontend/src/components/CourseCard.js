3576e1655a569c11a36a9f0a2db168aff4c9a254
OluwoleDove <ladmans07@gmail.com>
2024-08-13 09:22:51 +0100

    getting better with the frontend

--- edtech_scrapy/frontend/src/components/CourseCard.js
+++ edtech_scrapy/frontend/src/components/CourseCard.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 const CourseCard = ({ course }) => {
+    const formatPrice = (price) => {
+        if (price !== undefined && price !== null) {
+            return `₹${price.toLocaleString('en-IN')}`;
+        }
+        return 'Price not available';
+    };
+
     return (
-        <Link to={`/course/${course.id}`}> {/* Wrap the card in a Link */}
+        <Link to={`/course/${course.id}`}>
             <div className="course-card">
-                <img src={course.thumbnail} alt={course.title} />
+                <div className="image-container">
+                    <img src={course.thumbnail} alt={course.title} />
+                    <div className="overlay">
+                        <p className="description">{course.description || 'No description available'}</p>
+                    </div>
+                </div>
                 <h3>{course.title}</h3>
+                <p><strong>Tutor:&ensp;</strong> {course.tutor || 'Tutor not available'}</p>
+                <p><strong>Price:&ensp;</strong> {formatPrice(course.price)}</p>
                 <div className="rating">
-                    <span>{ course.rating } </span>
+                    <span>{course.rating}&ensp;</span>
                     {[...Array(5)].map((_, index) => (
                         <FaStar
                             key={index}
