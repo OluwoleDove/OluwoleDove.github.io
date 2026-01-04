7be4666ac3539c981e347066e587427a1c7ddc79
OluwoleDove <ladmans07@gmail.com>
2026-01-04 08:52:39 +0100

    Updating repo, removing old files

--- edtech_scrapy/backend/public/get_course_details.php
+++
@@ -1,8 +0,0 @@
-<?php
-// public/get_course_details.php
-
-require_once '../controllers/CourseController.php';
-
-$controller = new CourseController($connection);
-$controller->getCourseDetails();
-?>
